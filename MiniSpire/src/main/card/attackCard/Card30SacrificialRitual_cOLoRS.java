d1b1fedd7673fcaa67b515aa91186b712b1d2ab3
fqq864 <147639416+fqq864@users.noreply.github.com>
2025-11-14 18:13:50 +0800

    Add card30 "Sacrificial Ritual - cOLoRS"

---
+++ MiniSpire/src/main/card/attackCard/Card30SacrificialRitual_cOLoRS.java
@@ -0,0 +1,22 @@
+package main.card.attackCard;
+
+import main.processor.DamageProcessor;
+import main.enemy.Enemy;
+import main.player.Player;
+
+public class Card30SacrificialRitual_cOLoRS extends AttackCard {
+    
+    public Card30SacrificialRitual_cOLoRS() {
+        this.name = "Sacrificial Ritual - cOLoRS";
+        this.info = "Deal 100 damage. --Power demands sacrifice. Shall we begin?";
+        this.cost = 0;
+        this.rarity = "special";
+        this.baseDamage = 100;
+    }
+
+    @Override
+    public void onPlay(Player player, Enemy enemy) {
+        DamageProcessor.applyDamageToEnemy(baseDamage, Player.getInstance(), enemy);
+        player.lostColors();
+    }
+}

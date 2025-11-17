de9a06ad7ced52366c6708bab67c835ff5d3813b
fqq864 <147639416+fqq864@users.noreply.github.com>
2025-11-17 14:27:47 +0800

    Update NodeSanctuary.java

--- MiniSpire/src/main/node/NodeSanctuary.java
+++ MiniSpire/src/main/node/NodeSanctuary.java
@@ -39,14 +39,14 @@ public class NodeSanctuary extends Node {
 			hasHealed = true;
 			}, 1500, TimeUnit.MILLISECONDS);
 		}
-
+		
 		int getCard30RandNum = Main.random.nextInt(10);
 		switch (getCard30RandNum) {
 			case 0:
 				Card card30 = CardFactory.getInstance().createCard(30);
 				player.addCardToDeck(card30);
-				TextDisplay.printCharWithDelay("A flicker of light from the campfire's ashes catches your eye. There's something there.",10);
-				TextDisplay.printCharWithDelay("It's a... vibrantly colorful card. Yet when you look directly at it, a sharp pain stings your eyes. It seems as though everything around the card is losing its color.", 5);
+				TextDisplay.printCharWithDelay(" A flicker of light from the campfire's ashes catches your eye. There's something there.",10);
+				TextDisplay.printCharWithDelay(" It's a... vibrantly colorful card. Yet when you look directly at it, a sharp pain stings your eyes. It seems as though everything around the card is losing its color.", 5);
 				TextDisplay.printLineWithDelay(" >> Added " + Colors.colorOnForCardName(card30) + " to your deck!", 100);
 				break;
 			default:
@@ -120,4 +120,5 @@ public class NodeSanctuary extends Node {
 	public Boolean getHasChosen(){
 		return hasChosen;
 	}
+
 }

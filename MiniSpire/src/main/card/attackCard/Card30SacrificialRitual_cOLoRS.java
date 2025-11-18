b5792513bc10a61aa910b3c59167f3946ebea490
Lazzzzzlol <3476842650@qq.com>
2025-11-18 09:46:53 +0800

    修复色彩献祭的颜色消失效果没有立刻生效的问题

--- MiniSpire/src/main/card/attackCard/Card30SacrificialRitual_cOLoRS.java
+++ MiniSpire/src/main/card/attackCard/Card30SacrificialRitual_cOLoRS.java
@@ -18,8 +18,8 @@ public class Card30SacrificialRitual_cOLoRS extends AttackCard {
 
     @Override
     public void onPlay(Player player, Enemy enemy) {
-        DamageProcessor.applyDamageToEnemy(baseDamage, Player.getInstance(), enemy);
         player.lostColors();
+        DamageProcessor.applyDamageToEnemy(baseDamage, Player.getInstance(), enemy);
         TextDisplay.printLineWithDelay("The garish, colorful card dissolves into dead gray dust within your grasp, vanishing on the wind.", 50);
         TextDisplay.printLineWithDelay("You sense it — the world grows pale before your very eyes.", 50);
         TextDisplay.printLineWithDelay(" >> The card 'Sacrificial Ritual - cOLoRS' is gone, as if it had never been.", 50);

f6137387bef41cede8daef7fbe966b6b4ed24f7d
fqq864 <147639416+fqq864@users.noreply.github.com>
2025-11-18 17:07:30 +0800

    Add files via upload

---
+++ MiniSpire/src/main/buff/oneRoundBuff/BuffScurvy.java
@@ -0,0 +1,8 @@
+package main.buff.oneRoundBuff;
+
+public class BuffScurvy extends OneRoundBuff{
+    public BuffScurvy(int duration) {
+        this.name = "Scurvy";
+        this.duration = duration;
+    }
+}

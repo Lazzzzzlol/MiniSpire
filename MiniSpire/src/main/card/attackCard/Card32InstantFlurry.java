f778670da2d38ceb329b8b51a2af4164ca023403
fqq864 <147639416+fqq864@users.noreply.github.com>
2025-11-26 14:27:46 +0800

    Add files via upload

---
+++ MiniSpire/src/main/card/attackCard/Card32InstantFlurry.java
@@ -0,0 +1,23 @@
+package main.card.attackCard;
+
+import main.processor.DamageProcessor;
+import main.enemy.Enemy;
+import main.player.Player;
+
+public class Card32InstantFlurry extends AttackCard {
+    
+    public Card32InstantFlurry() {
+        this.name = "Instant Flurry";
+        this.info = "Deal 4 damage, draw 1 card.[Temporary] --Make haste, the wind waits for no one.";
+        this.cost = 0;
+        this.rarity = "special";
+        this.baseDamage = 4;
+        this.Temporary = true;
+    }
+
+    @Override
+    public void onPlay(Player player, Enemy enemy) {
+        DamageProcessor.applyDamageToEnemy(baseDamage, Player.getInstance(), enemy);
+        player.drawHandCards(1, null);
+    }
+}

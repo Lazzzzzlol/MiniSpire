3b94fcd7d50666864115c1238a506df6508513d8
fqq864 <147639416+fqq864@users.noreply.github.com>
2025-11-27 18:51:45 +0800

    Create BuffMentalOverload.java

---
+++ MiniSpire/src/main/buff/positiveBuff/BuffMentalOverload.java
@@ -0,0 +1,9 @@
+package main.buff.positiveBuff;
+
+public class BuffMentalOverload extends PositiveBuff {
+    
+    public BuffMentalOverload(int duration) {
+        this.name = "Mental Overload";
+        this.duration = duration;
+    }
+}
